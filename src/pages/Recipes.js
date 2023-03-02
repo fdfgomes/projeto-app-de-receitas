8af36e5566dfb0e059ef401eb88eec1b922cc6e7
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-03-02 20:26:31 -0300

    requisitos referentes ao componente footer

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 function Recipes() {
   return (
     <main>
       <p>Recipes</p>
+      <Footer />
     </main>
   );
 }
---
+++ src/tests/Footer.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../utility/renderWithRouter';
+
+describe('Testes referentes ao componente Footer.js', () => {
+  it('O footer deve ser renderizado na página /meals', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/meals');
+    });
+
+    const footer = screen.getByTestId('footer');
+
+    expect(footer).toBeInTheDocument();
+  });
+
+  it('O footer deve ser renderizado na página /drinks', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/drinks');
+    });
+
+    const footer = screen.getByTestId('footer');
+
+    expect(footer).toBeInTheDocument();
+  });
+
+  it('O footer deve ser renderizado na página /profile', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/profile');
+    });
+
+    const footer = screen.getByTestId('footer');
+
+    expect(footer).toBeInTheDocument();
+  });
+
+  it('Ao clicar nos ícones, deve realizar o devido redirecionamento', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/profile');
+    });
+
+    const mealsBtn = screen.getByTestId('meals-bottom-btn');
+    expect(mealsBtn).toBeInTheDocument();
+
+    userEvent.click(mealsBtn);
+    expect(history.location.pathname).toBe('/meals');
+
+    act(() => {
+      history.push('/profile');
+    });
+
+    const drinksBtn = screen.getByTestId('drinks-bottom-btn');
+    expect(drinksBtn).toBeInTheDocument();
+    userEvent.click(drinksBtn);
+    expect(history.location.pathname).toBe('/drinks');
+  });
+});

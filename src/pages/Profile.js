fbd1315f6f0c679423ad597b3447552740c44c9f
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-03-03 17:00:23 -0300

    implementação da página de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+
   return (
     <>
       <Header title="Profile" />
       <main>
-        <p>Profile</p>
+        <h2>Profile</h2>
+        <h4 data-testid="profile-email">{email}</h4>
+        <div>
+          <Link to="/done-recipes">
+            <button data-testid="profile-done-btn">Done Recipes</button>
+          </Link>
+          <Link to="/favorite-recipes">
+            <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+          </Link>
+          <Link to="/" onClick={ () => { localStorage.clear(); } }>
+            <button data-testid="profile-logout-btn">Logout</button>
+          </Link>
+        </div>
       </main>
       <Footer />
     </>
